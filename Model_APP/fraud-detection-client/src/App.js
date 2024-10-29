ebaca61fd687cc3ec75ed5f711933e9e8caf3cc1
Marta233 <martaassefa23@gmail.com>
2024-10-29 15:15:42 +0300

    model dep

---
+++ Model_APP/fraud-detection-client/src/App.js
@@ -0,0 +1,29 @@
+// src/App.js
+import React from 'react';
+import FraudForm from './components/FraudForm';
+import Nav from './components/nav/Nav';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Banner from './components/Banner/Banner';
+import './App.css';
+
+function App() {
+    return (
+        <Router>
+            <Nav /> {/* Move Nav outside of Routes for consistent display */}
+            <Routes>
+                <Route
+                    path="/"
+                    element={<FraudForm />}// Show Banner at root path
+                />
+                <Route
+                    path="/fraud-form" // Use a specific path for FraudForm
+                    element={<FraudForm />}
+                />
+            </Routes>
+        </Router>
+    );
+}
+
+export default App;
